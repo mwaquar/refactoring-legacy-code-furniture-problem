6e73e56aeefd0f88f3da4a467c7b4eef1504cce3
Waquar Usmani <waquar.usmani@thoughtworks.com>
2023-11-24 11:50:32 +0530

    Pass 1 - Add Test

--- src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
+++ src/test/java/com/thoughtworks/furniturerental/CustomerTest.java
@@ -6,11 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void testStatement(){
+        String expected = "Rental Record for John\n" +
+                "\tSofa\t350.0\n" +
+                "\tChair\t600.0\n" +
+                "Amount owed is 950.0\n" +
+                "You earned 3 frequent renter points";
 
-        int expected = 3;
+        Customer customer = new Customer("John");
+        Rental rental1 = new Rental(new Furniture("Sofa", Furniture.REGULAR), 3);
+        Rental rental2 = new Rental(new Furniture("Chair", Furniture.NEW_LAUNCH), 2);
 
-        assertEquals(expected, 1 + 2);
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+
+        assertEquals(expected, customer.statement());
     }
-
 }
